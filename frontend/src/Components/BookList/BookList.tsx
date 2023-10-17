11a5b4d08d7d58c68e403a562190105793546db3
Daran Dachte <darandachte@gmail.com>
2023-10-17 22:33:00 +0200

    work with state

---
+++ frontend/src/Components/BookList/BookList.tsx
@@ -0,0 +1,36 @@
+import { useSelector, useDispatch } from "react-redux";
+import { deleteBook } from "../../redux/books/actionCreators";
+import "./BookList.css";
+const BookList = () => {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const handleDeleteBook = (id) => {
+    dispatch(deleteBook(id));
+  };
+
+  return (
+    <div className="app-block book-list">
+      <h2>Book List</h2>
+      {books.length ? (
+        <ul>
+          {books.map((book, i) => (
+            <li key={book.id}>
+              <div className="book-info">
+                {++i}. {book.title} by <strong>{book.author}</strong>
+              </div>
+              <div className="book-actions">
+                <button onClick={() => handleDeleteBook(book.id)}>
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No books available</p>
+      )}
+    </div>
+  );
+};
+
+export default BookList;
